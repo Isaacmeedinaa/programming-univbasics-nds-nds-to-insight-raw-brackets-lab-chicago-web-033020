5c8cf6a590ef456fac1aa9f8c4f3d6878a5ca248
Isaac Medina <isaac.meedinaa@gmail.com>
2020-03-24 03:19:48 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,22 +7,24 @@ def directors_totals(nds)
   
   index1 = 0
   result = {}
+  
   while index1 < nds.length do
-    
-    
     director = nds[index1][:name]
     movies = nds[index1][:movies]
     
     index2 = 0
     total = 0
+    
     while index2 < movies.length do
       total = total + movies[index2][:worldwide_gross]
       index2 += 1
     end
+    
     result[director] = total
     
     index1 +=1
   end
+  
   nil
   result
 end
